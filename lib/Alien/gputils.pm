bc8905740b5807bda7abe9c77b89e91c084fa9ac
Vikas N Kumar <vikas@cpan.org>
2014-11-20 11:36:39 -0500

    upgrading version

--- lib/Alien/gputils.pm
+++ lib/Alien/gputils.pm
@@ -12,7 +12,7 @@ if ($^O =~ /mswin32/i) {
 use File::Which qw(which);
 use File::Spec;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 $VERSION = eval $VERSION;
 
 use parent 'Alien::Base';
@@ -66,7 +66,7 @@ The source code is taken from L<http://gputils.sourceforge.net>.
 
 =head1 VERSION
 
-0.05
+0.06
 
 =head1 METHODS
 
